4811c08de8ffb309557eb2e97cb07aa9981e606e
yuma-matsui <oshino.iiyo@gmail.com>
2021-02-23 16:21:02 +0900

    switch構文

---
+++ section3/sample19.php
@@ -0,0 +1,23 @@
+<?php
+  echo '色を選んでください(1.黒, 2.白, 3.赤) > ';
+  $color = (int)trim(fgets(STDIN));
+
+  // if($color === 1){
+  //   echo '黒が選ばれました';
+  // } elseif ($color === 2){
+  //   echo '白が選ばれました';
+  // } elseif($color === 3){
+  //   echo '赤が選ばれました';
+  // }
+
+  switch($color){
+    case 1:
+      echo '黒が選ばれました';
+      break;
+    case 2:
+      echo '白が選ばれました';
+      break;
+    case 3:
+      echo '赤が選ばれました';
+      break;
+  }
