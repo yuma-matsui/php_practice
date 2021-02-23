b0ac51eeea5571b4e2658138aefc5ffcd9bc33ca
yuma-matsui <oshino.iiyo@gmail.com>
2021-02-23 18:25:44 +0900

    do while構文

---
+++ section3/sample27.php
@@ -0,0 +1,14 @@
+<?php
+  // $dice = rand(1, 6);
+
+  // while($dice !== 1){
+  //   echo $dice . "\n";
+  //   $dice = rand(1, 6);
+  // }
+
+  // echo $dice;
+
+  do {
+    $dice = rand(1,6);
+    echo $dice, "\n";
+  } while ($dice !== 1);
