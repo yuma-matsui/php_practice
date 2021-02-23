75a0cb70c13cbd1b8a6a56f6d2c2b38aa2140105
yuma-matsui <oshino.iiyo@gmail.com>
2021-02-23 17:01:20 +0900

    連想配列

---
+++ section3/sample22.php
@@ -0,0 +1,12 @@
+<?php
+  // $prefs['hokkaido'] = '北海道';
+  // $prefs['aomori'] = '青森県';
+  // $prefs['iwate'] = '岩手県';
+
+  $prefs = [
+    'hokkaido' => '北海道',
+    'aomori' => '青森県',
+    'iwate' => '岩手県'
+  ];
+  
+  echo $prefs['iwate'];
