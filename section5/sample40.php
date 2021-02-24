6383d39492738cb5da26ec2b8f8c871f8812ff23
yuma-matsui <oshino.iiyo@gmail.com>
2021-02-24 12:50:28 +0900

    オブジェクト指向　クラス

---
+++ section5/sample40.php
@@ -0,0 +1,12 @@
+<?php
+  //タイムゾーンを合わせる関数
+  // date_default_timezone_set('Asia/Tokyo');
+
+  // $today = time();
+  // $format_date = date('y/m/d', $today);
+  // echo $format_date;
+
+  $today = new DateTime();
+  $today->setTimeZone(new DateTimeZone('Asia/Tokyo'));
+  $format_date = $today->format('Y/m/d');
+  echo $format_date;
