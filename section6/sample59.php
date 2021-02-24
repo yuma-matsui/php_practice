4e70d3b9c046dbeb1002e992316dd28bb6c82941
yuma-matsui <oshino.iiyo@gmail.com>
2021-02-24 16:36:24 +0900

    ファイルの読み込み

---
+++ section6/sample59.php
@@ -0,0 +1,13 @@
+<?php
+  $file = new SplFileObject('data/items.csv');
+
+  // while($file->eof() === false){
+  //   $item = $file->fgetcsv();
+  //   echo $item[0] . "\n";
+  // }
+
+  //フラグで処理する
+  $file->setFlags(SplFileObject::READ_CSV);
+  foreach($file as $item){
+    echo $item[0] . "\n";
+  }
